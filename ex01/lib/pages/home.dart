6a05f36002cc218b2bb692c0f489a67a8da183f2
mac_linux <maxence.isaac.c@gmail.com>
2026-06-15 16:39:54 +0200

    finishing exercice 02

--- ex01/lib/pages/home.dart
+++ ex01/lib/pages/home.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+    const HomePage({super.key});
 
-    const HomePage({
-        super.key,
-    });
+    @override
+    State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+    String text = "A simple text";
+
+    void changeText() {
+        if (text == "Text changed after click!")
+        {   
+            setState(() {
+                text = "A simple text";
+            });
+        }
+        else{ 
+            setState(() {
+                text = "Text changed after click!";
+            });
+        }
+    }
+
 
     @override
     Widget build(BuildContext context) {
         return Scaffold(
-            body: const Center(
-                child: Text('Hey flutter'),
+            body: Center(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        Text(
+                            text,
+                            style: TextStyle(
+                            fontSize: 26,
+                            fontWeight:  FontWeight.bold,
+                            color: const Color.fromARGB(255, 69, 212, 50) 
+                            ),
+                        ),
+                        ElevatedButton(
+                            onPressed: changeText, 
+                            child: const Text("Click"),
+                        ),
+                    ],
+                ),
             ),
         );
     }
