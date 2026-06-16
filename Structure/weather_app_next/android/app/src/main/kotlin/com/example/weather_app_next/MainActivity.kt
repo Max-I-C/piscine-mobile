251fd8bfa2b456c382f6d3683ebf505c4d07f359
Max <81183216+MoustikBe@users.noreply.github.com>
2026-06-16 17:05:39 +0200

    ex02

---
+++ Structure/weather_app_next/android/app/src/main/kotlin/com/example/weather_app_next/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weather_app_next
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Structure/weather_app_next/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Weather App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String city = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            onChanged: (value){
+              setState(() {
+                city = value;
+              });
+              print(city);
+            },
+            decoration: InputDecoration(
+              hintText: "Search a city",
+              border: InputBorder.none,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {}, 
+              icon: Icon(Icons.my_location)
+            ),
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text("Currently\n${city}")),
+            Center(child: Text("Today\n${city}")),
+            Center(child: Text("Weekly\n${city}")),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: TabBar(
+            tabs: [
+              Tab(text: "Currently", icon: Icon(Icons.timer)),
+              Tab(text: "Today", icon: Icon(Icons.today)),
+              Tab(text: "Weekly", icon: Icon(Icons.calendar_month)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
